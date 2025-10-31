42a6c62f67321571c51ac48e63eaa019109ad72a
Mushine Lament <mushineTabitha@outlook.com>
2025-10-31 15:36:43 +0800

    渲染翻转

--- crates/bevy_ui_render/src/lib.rs
+++ crates/bevy_ui_render/src/lib.rs
@@ -40,7 +40,7 @@ use bevy_core_pipeline::core_3d::graph::{Core3d, Node3d};
 use bevy_ecs::prelude::*;
 use bevy_ecs::system::SystemParam;
 use bevy_image::{prelude::*, TRANSPARENT_IMAGE_HANDLE};
-use bevy_math::{Affine2, FloatOrd, Mat4, Rect, UVec4, Vec2};
+use bevy_math::{Affine2, FloatOrd, Mat4, Rect, UVec4, Vec2, Vec3};
 use bevy_render::{
     render_asset::RenderAssets,
     render_graph::{Node as RenderGraphNode, NodeRunError, RenderGraph, RenderGraphContext},
@@ -1403,10 +1403,10 @@ impl Default for UiMeta {
 }
 
 pub(crate) const QUAD_VERTEX_POSITIONS: [Vec2; 4] = [
-    Vec2::new(-0.5, -0.5),
-    Vec2::new(0.5, -0.5),
-    Vec2::new(0.5, 0.5),
     Vec2::new(-0.5, 0.5),
+    Vec2::new(0.5, 0.5),
+    Vec2::new(0.5, -0.5),
+    Vec2::new(-0.5, -0.5),
 ];
 
 pub(crate) const QUAD_INDICES: [usize; 6] = [0, 2, 3, 0, 1, 2];
@@ -1697,7 +1697,7 @@ pub fn prepare_uinodes(
                         let transform = extracted_uinode.transform;
 
                         // Specify the corners of the node
-                        let positions = QUAD_VERTEX_POSITIONS
+                        let mut positions = QUAD_VERTEX_POSITIONS
                             .map(|pos| transform.transform_point2(pos * rect_size).extend(0.));
                         let points = QUAD_VERTEX_POSITIONS.map(|pos| pos * rect_size);
 
@@ -1726,12 +1726,17 @@ pub fn prepare_uinodes(
                             [Vec2::ZERO; 4]
                         };
 
+                        positions[0].y *= -1.0;
+                        positions[1].y *= -1.0;
+
                         let positions_clipped = [
                             positions[0] + positions_diff[0].extend(0.),
                             positions[1] + positions_diff[1].extend(0.),
                             positions[2] + positions_diff[2].extend(0.),
                             positions[3] + positions_diff[3].extend(0.),
                         ];
+
+                        tracing::info!("positions_clipped:{:?}", positions_clipped);
 
                         let points = [
                             points[0] + positions_diff[0],
@@ -1783,20 +1788,20 @@ pub fn prepare_uinodes(
                             }
                             [
                                 Vec2::new(
-                                    uinode_rect.min.x + positions_diff[0].x,
-                                    uinode_rect.min.y + positions_diff[0].y,
-                                ),
-                                Vec2::new(
-                                    uinode_rect.max.x + positions_diff[1].x,
-                                    uinode_rect.min.y + positions_diff[1].y,
+                                    uinode_rect.min.x + positions_diff[3].x,
+                                    uinode_rect.max.y + positions_diff[3].y,
                                 ),
                                 Vec2::new(
                                     uinode_rect.max.x + positions_diff[2].x,
                                     uinode_rect.max.y + positions_diff[2].y,
                                 ),
                                 Vec2::new(
-                                    uinode_rect.min.x + positions_diff[3].x,
-                                    uinode_rect.max.y + positions_diff[3].y,
+                                    uinode_rect.max.x + positions_diff[1].x,
+                                    uinode_rect.min.y + positions_diff[1].y,
+                                ),
+                                Vec2::new(
+                                    uinode_rect.min.x + positions_diff[0].x,
+                                    uinode_rect.min.y + positions_diff[0].y,
                                 ),
                             ]
                             .map(|pos| pos / atlas_extent)
@@ -1828,106 +1833,106 @@ pub fn prepare_uinodes(
                         indices_index += 4;
                     }
                     ExtractedUiItem::Glyphs { range } => {
-                        let image = gpu_images
-                            .get(extracted_uinode.image)
-                            .expect("Image was checked during batching and should still exist");
+                        // let image = gpu_images
+                        //     .get(extracted_uinode.image)
+                        //     .expect("Image was checked during batching and should still exist");
 
-                        let atlas_extent = image.size_2d().as_vec2();
+                        // let atlas_extent = image.size_2d().as_vec2();
 
-                        for glyph in &extracted_uinodes.glyphs[range.clone()] {
-                            let color = glyph.color.to_f32_array();
-                            let glyph_rect = glyph.rect;
-                            let rect_size = glyph_rect.size();
+                        // for glyph in &extracted_uinodes.glyphs[range.clone()] {
+                        //     let color = glyph.color.to_f32_array();
+                        //     let glyph_rect = glyph.rect;
+                        //     let rect_size = glyph_rect.size();
 
-                            // Specify the corners of the glyph
-                            let positions = QUAD_VERTEX_POSITIONS.map(|pos| {
-                                extracted_uinode
-                                    .transform
-                                    .transform_point2(glyph.translation + pos * glyph_rect.size())
-                                    .extend(0.)
-                            });
+                        //     // Specify the corners of the glyph
+                        //     let positions = QUAD_VERTEX_POSITIONS.map(|pos| {
+                        //         extracted_uinode
+                        //             .transform
+                        //             .transform_point2(glyph.translation + pos * glyph_rect.size())
+                        //             .extend(0.)
+                        //     });
 
-                            let positions_diff = if let Some(clip) = extracted_uinode.clip {
-                                [
-                                    Vec2::new(
-                                        f32::max(clip.min.x - positions[0].x, 0.),
-                                        f32::max(clip.min.y - positions[0].y, 0.),
-                                    ),
-                                    Vec2::new(
-                                        f32::min(clip.max.x - positions[1].x, 0.),
-                                        f32::max(clip.min.y - positions[1].y, 0.),
-                                    ),
-                                    Vec2::new(
-                                        f32::min(clip.max.x - positions[2].x, 0.),
-                                        f32::min(clip.max.y - positions[2].y, 0.),
-                                    ),
-                                    Vec2::new(
-                                        f32::max(clip.min.x - positions[3].x, 0.),
-                                        f32::min(clip.max.y - positions[3].y, 0.),
-                                    ),
-                                ]
-                            } else {
-                                [Vec2::ZERO; 4]
-                            };
+                        //     let positions_diff = if let Some(clip) = extracted_uinode.clip {
+                        //         [
+                        //             Vec2::new(
+                        //                 f32::max(clip.min.x - positions[0].x, 0.),
+                        //                 f32::max(clip.min.y - positions[0].y, 0.),
+                        //             ),
+                        //             Vec2::new(
+                        //                 f32::min(clip.max.x - positions[1].x, 0.),
+                        //                 f32::max(clip.min.y - positions[1].y, 0.),
+                        //             ),
+                        //             Vec2::new(
+                        //                 f32::min(clip.max.x - positions[2].x, 0.),
+                        //                 f32::min(clip.max.y - positions[2].y, 0.),
+                        //             ),
+                        //             Vec2::new(
+                        //                 f32::max(clip.min.x - positions[3].x, 0.),
+                        //                 f32::min(clip.max.y - positions[3].y, 0.),
+                        //             ),
+                        //         ]
+                        //     } else {
+                        //         [Vec2::ZERO; 4]
+                        //     };
 
-                            let positions_clipped = [
-                                positions[0] + positions_diff[0].extend(0.),
-                                positions[1] + positions_diff[1].extend(0.),
-                                positions[2] + positions_diff[2].extend(0.),
-                                positions[3] + positions_diff[3].extend(0.),
-                            ];
+                        //     let positions_clipped = [
+                        //         positions[0] + positions_diff[0].extend(0.),
+                        //         positions[1] + positions_diff[1].extend(0.),
+                        //         positions[2] + positions_diff[2].extend(0.),
+                        //         positions[3] + positions_diff[3].extend(0.),
+                        //     ];
 
-                            // cull nodes that are completely clipped
-                            let transformed_rect_size =
-                                extracted_uinode.transform.transform_vector2(rect_size);
-                            if positions_diff[0].x - positions_diff[1].x
-                                >= transformed_rect_size.x.abs()
-                                || positions_diff[1].y - positions_diff[2].y
-                                    >= transformed_rect_size.y.abs()
-                            {
-                                continue;
-                            }
+                        //     // cull nodes that are completely clipped
+                        //     let transformed_rect_size =
+                        //         extracted_uinode.transform.transform_vector2(rect_size);
+                        //     if positions_diff[0].x - positions_diff[1].x
+                        //         >= transformed_rect_size.x.abs()
+                        //         || positions_diff[1].y - positions_diff[2].y
+                        //             >= transformed_rect_size.y.abs()
+                        //     {
+                        //         continue;
+                        //     }
 
-                            let uvs = [
-                                Vec2::new(
-                                    glyph.rect.min.x + positions_diff[0].x,
-                                    glyph.rect.min.y + positions_diff[0].y,
-                                ),
-                                Vec2::new(
-                                    glyph.rect.max.x + positions_diff[1].x,
-                                    glyph.rect.min.y + positions_diff[1].y,
-                                ),
-                                Vec2::new(
-                                    glyph.rect.max.x + positions_diff[2].x,
-                                    glyph.rect.max.y + positions_diff[2].y,
-                                ),
-                                Vec2::new(
-                                    glyph.rect.min.x + positions_diff[3].x,
-                                    glyph.rect.max.y + positions_diff[3].y,
-                                ),
-                            ]
-                            .map(|pos| pos / atlas_extent);
+                        //     let uvs = [
+                        //         Vec2::new(
+                        //             glyph.rect.min.x + positions_diff[0].x,
+                        //             glyph.rect.min.y + positions_diff[0].y,
+                        //         ),
+                        //         Vec2::new(
+                        //             glyph.rect.max.x + positions_diff[1].x,
+                        //             glyph.rect.min.y + positions_diff[1].y,
+                        //         ),
+                        //         Vec2::new(
+                        //             glyph.rect.max.x + positions_diff[2].x,
+                        //             glyph.rect.max.y + positions_diff[2].y,
+                        //         ),
+                        //         Vec2::new(
+                        //             glyph.rect.min.x + positions_diff[3].x,
+                        //             glyph.rect.max.y + positions_diff[3].y,
+                        //         ),
+                        //     ]
+                        //     .map(|pos| pos / atlas_extent);
 
-                            for i in 0..4 {
-                                ui_meta.vertices.push(UiVertex {
-                                    position: positions_clipped[i].into(),
-                                    uv: uvs[i].into(),
-                                    color,
-                                    flags: shader_flags::TEXTURED | shader_flags::CORNERS[i],
-                                    radius: [0.0; 4],
-                                    border: [0.0; 4],
-                                    size: rect_size.into(),
-                                    point: [0.0; 2],
-                                });
-                            }
+                        //     for i in 0..4 {
+                        //         ui_meta.vertices.push(UiVertex {
+                        //             position: positions_clipped[i].into(),
+                        //             uv: uvs[i].into(),
+                        //             color,
+                        //             flags: shader_flags::TEXTURED | shader_flags::CORNERS[i],
+                        //             radius: [0.0; 4],
+                        //             border: [0.0; 4],
+                        //             size: rect_size.into(),
+                        //             point: [0.0; 2],
+                        //         });
+                        //     }
 
-                            for &i in &QUAD_INDICES {
-                                ui_meta.indices.push(indices_index + i as u32);
-                            }
+                        //     for &i in &QUAD_INDICES {
+                        //         ui_meta.indices.push(indices_index + i as u32);
+                        //     }
 
-                            vertices_index += 6;
-                            indices_index += 4;
-                        }
+                        //     vertices_index += 6;
+                        //     indices_index += 4;
+                        // }
                     }
                 }
                 existing_batch.unwrap().1.range.end = vertices_index;
